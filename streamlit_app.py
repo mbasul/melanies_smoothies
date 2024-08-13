8cd706539c81b0244d73628bb9742398d84a0482
mbasul <matthias.balzer@sulzer.de>
2024-08-13 14:41:50 +0200

    Update streamlit_app.py
    HoET B03 L10 Kap 04 C

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
-        #st.subheader(fruit_choosen + ' Nutrition Informaton')
+        st.subheader(fruit_choosen + ' Nutrition Informaton')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
