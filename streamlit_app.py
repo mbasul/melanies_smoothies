c8bdc0594f2e5105247895af6fda6255499d97de
mbasul <matthias.balzer@sulzer.de>
2024-08-13 15:49:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ res = my_dataframe.collect()
 for R in res:
     F = R['FRUIT_NAME']
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+F)
-    st.write("https://fruityvice.com/api/fruit/"+F)
+    st.write(F+fruityvice_response)
 
 # -------------------------------------------------------------------
 ingredients_list = st.multiselect(
